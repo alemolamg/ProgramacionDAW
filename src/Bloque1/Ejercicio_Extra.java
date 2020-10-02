499661828676553b490c973fe7aa6fb699798b12
alemol <alemol@Alemol-MSI>
2020-10-02 15:36:05 +0200

    Ejercicio_Extra, comentarios añadidos

--- src/Bloque1/Ejercicio_Extra.java
+++ src/Bloque1/Ejercicio_Extra.java
@@ -12,7 +12,7 @@ public class Ejercicio_Extra {
 		System.out.println("Introduce el radio del círculo en cm: ");
 		radio = leer.nextDouble();
 		
-		area = Math.PI * Math.pow(radio, 2);
+		area = Math.PI * Math.pow(radio, 2);	//Fórmula para calcular el área
 		
 		System.out.println("\nEl área de un círculo de radio = " + radio + "cm es: " + area + "cm²");
 		
